d2d4031a86d0b5cb6bcef2d561b4fd65868f1a28
luisz2310 <luisz2310@hotmail.com>
2026-05-21 23:25:19 -0500

    tareas

---
+++ Unidad_1_Java_Basico/tareas/factorial.java
@@ -0,0 +1,15 @@
+package tareas;
+
+public class factorial {
+    // factorial de 5 
+
+    public static void main(String[] args){
+        int n=10;
+        int fac=1;
+        for(int i=1; i<=n; i++){
+            fac=fac*i;
+    }
+    System.out.println("Factorial es: "+fac);
+
+}
+}
---
+++ Unidad_1_Java_Basico/tareas/multiplicarNumeros.java
@@ -0,0 +1,13 @@
+package tareas;
+
+public class multiplicarNumeros {
+    public static void main(String[] args){
+int n1=5;
+int n2=8;
+int suma=0;
+for(int i=1; i<=n2; i++){
+    suma=suma+n1;
+    }
+    System.out.println("Resultado "+suma);
+}
+}
