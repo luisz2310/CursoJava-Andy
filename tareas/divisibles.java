8b154de883e3b56980bb964519f7a79dacc0a9d9
luisz2310 <luisz2310@hotmail.com>
2026-05-28 23:00:22 -0500

    tarea divisibles

---
+++ tareas/divisibles.java
@@ -0,0 +1,13 @@
+public class divisibles {
+public static void main(String[] args) throws Exception {
+        int n1=13;
+        int n2=4;
+        int div=0;
+         for(int i=1; i<=n1; i++){
+            div=i%n2;
+            if (div==0) {
+                System.out.println(i);
+            }         
+}
+}
+}
